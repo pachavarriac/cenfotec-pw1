8921360b9abda0833a6370c00be637da0b773464
pachavarriac <pachavarriac@ucenfotec.ac.cr>
2025-04-03 09:19:14 -0600

    Code refactor 03/04/2024 09:19

---
+++ proyecto_final/js/app.js
@@ -0,0 +1,46 @@
+const container = document.getElementById("app-container");
+const panorama = new PANOLENS.ImagePanorama("../images/car_workshop_pan.png");
+
+const balanceador_llantas = new PANOLENS.Infospot(50, PANOLENS.DataImage.Info);
+balanceador_llantas.position.set(492, 44, -500);
+balanceador_llantas.addHoverText("", -60);
+balanceador_llantas.element.innerHTML =
+  '<div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 300px;"><h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">Balanceador de llantas</><p style="font-size: 14px;">Una máquina de balanceo de llantas es un equipo diseñado para detectar y corregir desequilibrios en las ruedas de un vehículo. Funciona mediante sensores que analizan la distribución del peso en la rueda y determinan dónde se deben colocar contrapesos para equilibrarla.</p></div>';
+
+const destalonadora = new PANOLENS.Infospot(50, PANOLENS.DataImage.Info);
+destalonadora.position.set(302 ,-155 , -500);
+destalonadora.addHoverText("", -60);
+destalonadora.element.innerHTML =
+  '<div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 300px;"><h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">Destalonadora</><p style="font-size: 14px;">La máquina destalonadora de llantas es un equipo diseñado para desmontar y montar neumáticos en rines de manera rápida y eficiente. Su función principal es separar el neumático de la llanta sin dañarlo, facilitando el proceso de cambio, reparación o mantenimiento.</p></div>';
+
+const extractor_liquido_frenos = new PANOLENS.Infospot(50, PANOLENS.DataImage.Info);
+extractor_liquido_frenos.position.set(-124 ,-123 , -500);
+extractor_liquido_frenos.addHoverText("", -60);
+extractor_liquido_frenos.element.innerHTML =
+  '<div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 400px;"><h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">Extractor de Liquido de Frenos</><p style="font-size: 14px;">La máquina extractora de líquido de frenos es un equipo diseñado para realizar la extracción y reemplazo del fluido de frenos en vehículos de manera eficiente y segura. Su función principal es eliminar el líquido viejo y contaminado del sistema de frenos, evitando la presencia de humedad y aire, lo que garantiza un frenado óptimo y prolonga la vida útil de los componentes del sistema.</p></div>';
+
+const alineacion_direccion = new PANOLENS.Infospot(50, PANOLENS.DataImage.Info);
+alineacion_direccion.position.set(-500 ,97 , 19);
+alineacion_direccion.addHoverText("", -60);
+alineacion_direccion.element.innerHTML =
+  '<div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 400px;"><h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">Alineador de dirección</><p style="font-size: 14px;">Una máquina de alineación de dirección es un equipo utilizado para medir y ajustar los ángulos de las ruedas de un vehículo, asegurando que estén correctamente alineadas según las especificaciones del fabricante. Su función principal es optimizar la estabilidad, el control y la vida útil de los neumáticos, evitando desgastes irregulares y mejorando la eficiencia del combustible.</p></div>';
+
+const elevador_hidraulico = new PANOLENS.Infospot(50, PANOLENS.DataImage.Info);
+elevador_hidraulico.position.set(-500 ,-38 , 468);
+elevador_hidraulico.addHoverText("", -60);
+elevador_hidraulico.element.innerHTML =
+  '<div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 400px;"><h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">Elevador hidraulico</><p style="font-size: 14px;">Un elevador hidráulico de plataforma es un equipo utilizado en talleres mecánicos para elevar vehículos de manera segura y estable, facilitando tareas de mantenimiento, reparación y alineación. Funciona mediante un sistema hidráulico que acciona cilindros de elevación, permitiendo al operador levantar el automóvil con poco esfuerzo.</p></div>';
+
+panorama.add(balanceador_llantas);
+panorama.add(destalonadora);
+panorama.add(extractor_liquido_frenos);
+panorama.add(alineacion_direccion);
+panorama.add(elevador_hidraulico);
+
+viewer = new PANOLENS.Viewer({
+  container: container,
+  output: "fullscreen",
+  autohideInfospot: false,
+});
+
+viewer.add(panorama);
--- proyecto_final/js/config.js
+++ proyecto_final/js/config.js
@@ -2,148 +2,233 @@
 // Configuracion de credenciales
 // Configuracion de eventos para el formulario de login
 const credentials = {
-    username: 'cenfo',
-    password: '123'
+  username: "cenfo",
+  password: "123",
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-    const usernameInput = document.getElementById('username');
-    const passwordInput = document.getElementById('password');
-    const usernameLabel = document.querySelector('label[for="username"] span');
-    const passwordLabel = document.querySelector('label[for="password"] span');
-    const loginButton = document.getElementById('login-button');
+document.addEventListener("DOMContentLoaded", function () {
+  const usernameInput = document.getElementById("username");
+  const passwordInput = document.getElementById("password");
+  const usernameLabel = document.querySelector('label[for="username"] span');
+  const passwordLabel = document.querySelector('label[for="password"] span');
+  const loginButton = document.getElementById("login-button");
 
-    usernameInput.addEventListener('focus', function() {
-        usernameLabel.textContent = 'Usuario';
-    });
+  passwordInput.addEventListener("focus", function () {
+    passwordLabel.textContent = "Contraseña";
+  });
 
-    usernameInput.addEventListener('blur', function() {
-        if (!usernameInput.value) {
-            usernameLabel.textContent = 'Ingrese su usuario';
-        }
-    });
-
-    passwordInput.addEventListener('focus', function() {
-        passwordLabel.textContent = 'Contraseña';
-    });
-
-    passwordInput.addEventListener('blur', function() {
-        if (!passwordInput.value) {
-            passwordLabel.textContent = 'Ingrese su contraseña';
-        }
-    });
-
-    function showError(icon, title, message, popup, color) {
-        Swal.fire({
-            html: '<div style="display: flex; align-items: center; padding: 0; margin: 0;">'+
-            '<lottie-player src="'+icon+'" background="transparent"  speed="1"  style="width: 120px; height: 120px;" loop autoplay></lottie-player>'+
-            '<div style="display: flex; flex-direction: column; align-items: center; justify-content: center;  margin: 0px 0px 0px 20px; padding: 0;">'+
-            '<p style="font-family: Arial, Helvetica, sans-serif; font-size: 32px; color: '+color+'; margin: 5px;">'+title+'</p>'+
-            '<p style="font-family: Arial, Helvetica, sans-serif; font-size: 24px; margin: 5px;">'+message+'</p></div></div>',
-            showConfirmButton: false,
-            timer: 3000,
-            customClass: {
-                popup: popup
-            }
-        });
+  passwordInput.addEventListener("blur", function () {
+    if (!passwordInput.value) {
+      passwordLabel.textContent = "Ingrese su contraseña";
     }
+  });
 
-    function clearInputs() {
-        usernameInput.value = '';
-        passwordInput.value = '';
-        usernameLabel.textContent = 'Ingrese su usuario';
-        passwordLabel.textContent = 'Ingrese su contraseña';
-    }
-
-    loginButton.addEventListener('click', function(event) {
-        event.preventDefault();
-        const username = usernameInput.value;
-        const password = passwordInput.value;
-
-        if (!username && !password) {
-            showError('animations/credentials.json', 'Error', 'Ingrese su usuario y contraseña', 'custom-swal-popup-error', 'red');
-        } else if (!username) {
-            showError('animations/username.json', 'Error', 'Ingrese su usuario', 'custom-swal-popup-error', 'red');
-        } else if (!password) {
-            showError('animations/password.json', 'Error', 'Ingrese su contraseña', 'custom-swal-popup-error', 'red');
-        } else {
-            if (username === credentials.username && password === credentials.password) {
-                localStorage.setItem('loginSuccess', 'true'); 
-                window.location.href = 'landing.html';
-                clearInputs();
-            } else {
-                showError('animations/error-triangle.json', 'Error', 'Credenciales incorrectas Intente nuevamente', 'custom-swal-popup-error', 'red');
-                clearInputs();
-            }
-        }
+  function showError(icon, title, message, popup, color) {
+    Swal.fire({
+      html:
+        '<div style="display: flex; align-items: center; padding: 0; margin: 0;">' +
+        '<lottie-player src="' +
+        icon +
+        '" background="transparent"  speed="1"  style="width: 120px; height: 120px;" loop autoplay></lottie-player>' +
+        '<div style="display: flex; flex-direction: column; align-items: center; justify-content: center;  margin: 0px 0px 0px 20px; padding: 0;">' +
+        '<p style="font-family: Arial, Helvetica, sans-serif; font-size: 32px; color: ' +
+        color +
+        '; margin: 5px;">' +
+        title +
+        "</p>" +
+        '<p style="font-family: Arial, Helvetica, sans-serif; font-size: 24px; margin: 5px;">' +
+        message +
+        "</p></div></div>",
+      showConfirmButton: false,
+      timer: 3000,
+      customClass: {
+        popup: popup,
+      },
     });
+  }
 
-});
+  function clearInputs() {
+    usernameInput.value = "";
+    passwordInput.value = "";
+    usernameLabel.textContent = "Ingrese su usuario";
+    passwordLabel.textContent = "Ingrese su contraseña";
+  }
 
-document.addEventListener('DOMContentLoaded', function() {
-    if (localStorage.getItem('loginSuccess') === 'true') {
-        Swal.fire({
-            html: '<div style="display: flex; align-items: center; padding: 0px; margin: 0;">' +
-            '<lottie-player src="animations/success.json" background="transparent" speed="1" style="width: 100px; height: 100px;" loop autoplay></lottie-player>' +
-            '<div style="display: flex; flex-direction: column; align-items: center; justify-content: center; padding-left: 20px;">' +
-            '<p style="font-family: Arial, Helvetica, sans-serif; font-size: 32px; color: green; padding: 0px; margin: 5px;">Bienvenido</p>' +
-            '<p style="font-family: Arial, Helvetica, sans-serif; font-size: 24px; padding: 0px; margin: 5px;">Se ha logueado con éxito</p></div></div>',
-            showConfirmButton: false,
-            timer: 3000,
-            customClass: {
-                popup: 'custom-swal-popup-success'
-            }
-        });
+  loginButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const username = usernameInput.value;
+    const password = passwordInput.value;
 
-        localStorage.removeItem('loginSuccess');
-    }
-});
-
-window.addEventListener('scroll', function() {
-    const nav = document.querySelector('nav');
-    const logo = document.querySelector('.menu-logo');
-    if (window.scrollY > 0) {
-        [nav, logo].forEach(el => el.classList.add('scrolled'));
+    if (!username && !password) {
+      showError(
+        "animations/credentials.json",
+        "Error",
+        "Ingrese su usuario y contraseña",
+        "custom-swal-popup-error",
+        "red"
+      );
+    } else if (!username) {
+      showError(
+        "animations/username.json",
+        "Error",
+        "Ingrese su usuario",
+        "custom-swal-popup-error",
+        "red"
+      );
+    } else if (!password) {
+      showError(
+        "animations/password.json",
+        "Error",
+        "Ingrese su contraseña",
+        "custom-swal-popup-error",
+        "red"
+      );
     } else {
-        [nav, logo].forEach(el => el.classList.remove('scrolled'));
+      if (
+        username === credentials.username &&
+        password === credentials.password
+      ) {
+        localStorage.setItem("loginSuccess", "true");
+        window.location.href = "landing.html";
+        clearInputs();
+      } else {
+        showError(
+          "animations/error-triangle.json",
+          "Error",
+          "Credenciales incorrectas Intente nuevamente",
+          "custom-swal-popup-error",
+          "red"
+        );
+        clearInputs();
+      }
     }
+  });
 });
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    const inicioLink = document.querySelector('a[href="#inicio"]');
-    
-    // if (inicioLink) {
-        inicioLink.addEventListener('click', function(event) {
-            event.preventDefault();
-
-            const nav = document.querySelector('nav');
-            const logo = document.querySelector('.menu-logo');
-
-            [nav, logo].forEach(n => n.classList.remove('scrolled'));
-        });
-    // }
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('.menu a');
-    const headerHeight = document.querySelector('header').offsetHeight;
-    const navHeight = document.querySelector('nav').offsetHeight;
-
-    links.forEach(link => {
-        link.addEventListener('click', function(event) {
-            if (this.getAttribute('href').startsWith('#')) {
-                event.preventDefault();
-                const targetId = this.getAttribute('href').substring(1);
-                const targetElement = document.getElementById(targetId);
-                if (targetElement) {
-                    const offsetPosition = targetElement.offsetTop - headerHeight - navHeight;
-                    window.scrollTo({
-                        top: offsetPosition,
-                        behavior: 'smooth'
-                    });
-                }
-            }
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("loginSuccess") === "true") {
+    Swal.fire({
+      html:
+        '<div style="display: flex; align-items: center; padding: 0px; margin: 0;">' +
+        '<lottie-player src="animations/success.json" background="transparent" speed="1" style="width: 100px; height: 100px;" loop autoplay></lottie-player>' +
+        '<div style="display: flex; flex-direction: column; align-items: center; justify-content: center; padding-left: 20px;">' +
+        '<p style="font-family: Arial, Helvetica, sans-serif; font-size: 32px; color: green; padding: 0px; margin: 5px;">Bienvenido</p>' +
+        '<p style="font-family: Arial, Helvetica, sans-serif; font-size: 24px; padding: 0px; margin: 5px;">Se ha logueado con éxito</p></div></div>',
+      showConfirmButton: false,
+      timer: 3000,
+      customClass: {
+        popup: "custom-swal-popup-success",
+      },
     });
+
+    localStorage.removeItem("loginSuccess");
+  }
+});
+
+window.addEventListener("scroll", function () {
+  const nav = document.querySelector("nav");
+  const logo = document.querySelector(".ml");
+  if (window.scrollY > 0) {
+    [nav, logo].forEach((el) => el.classList.add("scrolled"));
+  } else {
+    [nav, logo].forEach((el) => el.classList.remove("scrolled"));
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const inicioLink = document.querySelector('a[href="#inicio"]');
+
+  if (inicioLink) {
+    inicioLink.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const nav = document.querySelector("nav");
+      const logo = document.querySelector(".ml");
+
+      [nav, logo].forEach((n) => n.classList.remove("scrolled"));
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const firstMenuItem = document.querySelector(".mc a");
+
+    if (firstMenuItem) {
+      firstMenuItem.classList.add("lip");
+    	firstMenuItem.focus();
+    }
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".menu a");
+  const headerHeight = document.querySelector("header").offsetHeight;
+  const navHeight = document.querySelector("nav").offsetHeight;
+
+  links.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      if (this.getAttribute("href").startsWith("#")) {
+        event.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+          const offsetPosition =
+            targetElement.offsetTop - headerHeight - navHeight;
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth",
+          });
+        }
+      }
+    });
+  });
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  let sections = document.querySelectorAll("section");
+  let menuItems = document.querySelectorAll(".lip a");
+
+  function setActiveLink(targetId) {
+      menuItems.forEach(item => {
+          item.classList.remove("active");
+          if (item.getAttribute("href") === `#${targetId}`) {
+              item.classList.add("active");
+          }
+      });
+  }
+
+  // Evento de clic en los enlaces del menú
+  menuItems.forEach(item => {
+      item.addEventListener("click", function (e) {
+          e.preventDefault();
+          let targetId = this.getAttribute("href").substring(1);
+          let targetSection = document.getElementById(targetId);
+
+          // Desplazamiento suave a la sección
+          window.scrollTo({
+              top: targetSection.offsetTop - 50,
+              behavior: "smooth"
+          });
+
+          // Cambiar el borde en el menú
+          setActiveLink(targetId);
+      });
+  });
+
+  // Detectar la sección visible en el viewport al hacer scroll
+  window.addEventListener("scroll", function () {
+      let scrollPosition = window.scrollY;
+
+      sections.forEach(section => {
+          let sectionTop = section.offsetTop - 60; // Ajusta según el tamaño del menú
+          let sectionHeight = section.offsetHeight;
+          let sectionId = section.getAttribute("id");
+
+          if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+              setActiveLink(sectionId);
+          }
+      });
+  });
 });
