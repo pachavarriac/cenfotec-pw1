8cf31dc6e164c600fe0f6c6e0a31943503afa852
pachavarriac <pachavarriac@ucenfotec.ac.cr>
2025-04-09 18:18:38 -0600

    09/04/2025 18:18

--- proyecto_final/js/app.js
+++ proyecto_final/js/app.js
@@ -7,19 +7,29 @@ function goBack() {
   }
 }
 
+const closeButtons = document.querySelectorAll('.close');
+closeButtons.forEach(button => {
+  button.addEventListener('click', function() {
+    workshop_front_container.style.display = "none";
+  });
+});
+
 const container = document.getElementById("app-container");
+const workshop_front_img = document.getElementById("workshop_front_img");
+const workshop_front_alt = document.getElementById("workshop_front_alt");
+const workshop_front_container = document.getElementById("workshop_front_container");
 const panorama = new PANOLENS.ImagePanorama("../images/panolense/car_workshop_pan.png");
 
 const balanceador_llantas = new PANOLENS.Infospot(64, 'images/panolense/tire-balance.png');
 balanceador_llantas.position.set(500 ,-69 , -499);
 balanceador_llantas.addHoverText("", -260);
 balanceador_llantas.element.innerHTML = `
-  <div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 300px;">
-    <h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">
+  <div style="background-color: rgba(0, 0, 0, 0.8); border: 1px solid rgba(255, 255, 255, 0.8); color: #fff; border-radius: 15px; width: 400px;">
+    <h2 style="text-align: center; font-size: 22px; padding: 20px; width: 100%; background-color: white; color: black; border-radius: 15px 15px 0px 0px;">
       Balanceador de llantas
     <h2/>
-    <p style="font-size: 14px;">
-      Una máquina de balanceo de llantas es un equipo diseñado para detectar y corregir desequilibrios en las ruedas de un vehículo. Funciona mediante sensores que analizan la distribución del peso en la rueda y determinan dónde se deben colocar contrapesos para equilibrarla.
+    <p style="font-size: 16px; padding: 24px; text-align: justified;">
+      Diseñada para detectar desequilibrios en las ruedas y garantizar un rendimiento óptimo del vehículo. Asegura una conducción suave y reduce el desgaste irregular de los neumáticos.
     </p>
   </div>
 `;
@@ -28,12 +38,12 @@ const destalonadora = new PANOLENS.Infospot(64, 'images/panolense/tire-pressure-
 destalonadora.position.set(302, -155, -500);
 destalonadora.addHoverText("", 60);
 destalonadora.element.innerHTML = `
-  <div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 300px;">
-    <h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">
+  <div style="background-color: rgba(0, 0, 0, 0.8); border: 1px solid rgba(255, 255, 255, 0.8); color: #fff; border-radius: 15px; width: 400px;">
+    <h2 style="text-align: center; font-size: 22px; padding: 20px; width: 100%; background-color: white; color: black; border-radius: 15px 15px 0px 0px;">
       Destalonadora
     <h2/>
-    <p style="font-size: 14px;">
-      La máquina destalonadora de llantas es un equipo diseñado para desmontar y montar neumáticos en rines de manera rápida y eficiente. Su función principal es separar el neumático de la llanta sin dañarlo, facilitando el proceso de cambio, reparación o mantenimiento.
+    <p style="font-size: 16px; padding: 24px; text-align: justified;">
+      Equipo diseñado para desmontar neumáticos de forma rápida y segura. Facilita la separación del talón de la llanta sin dañar la rueda, optimizando el trabajo en talleres de llantas y mecánica.
     </p>
   </div>
 `;
@@ -42,12 +52,12 @@ const extractor_liquido_frenos = new PANOLENS.Infospot(64, 'images/panolense/bra
 extractor_liquido_frenos.position.set(-124, -123, -500);
 extractor_liquido_frenos.addHoverText("", 60);
 extractor_liquido_frenos.element.innerHTML = `
-  <div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 400px;">
-    <h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">
+  <div style="background-color: rgba(0, 0, 0, 0.8); border: 1px solid rgba(255, 255, 255, 0.8); color: #fff; border-radius: 15px; width: 400px;">
+    <h2 style="text-align: center; font-size: 22px; padding: 20px; width: 100%; background-color: white; color: black; border-radius: 15px 15px 0px 0px;">
       Extractor de Liquido de Frenos
     <h2/>
-    <p style="font-size: 14px;">
-      La máquina extractora de líquido de frenos es un equipo diseñado para realizar la extracción y reemplazo del fluido de frenos en vehículos de manera eficiente y segura. Su función principal es eliminar el líquido viejo y contaminado del sistema de frenos, evitando la presencia de humedad y aire, lo que garantiza un frenado óptimo y prolonga la vida útil de los componentes del sistema.
+    <p style="font-size: 16px; padding: 24px; text-align: justified;">
+      Permite una extracción rápida, limpia y segura del fluido de frenos, facilitando el mantenimiento del sistema hidráulico sin contaminar componentes.
     </p>
   </div>
 `;
@@ -56,12 +66,12 @@ const alineacion_direccion = new PANOLENS.Infospot(64, 'images/panolense/wheel-a
 alineacion_direccion.position.set(-500, 97, 19);
 alineacion_direccion.addHoverText("", -270);
 alineacion_direccion.element.innerHTML = `
-  <div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 400px;">
-    <h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">
+  <div style="background-color: rgba(0, 0, 0, 0.8); border: 1px solid rgba(255, 255, 255, 0.8); color: #fff; border-radius: 15px; width: 400px;">
+    <h2 style="text-align: center; font-size: 22px; padding: 20px; width: 100%; background-color: white; color: black; border-radius: 15px 15px 0px 0px;">
       Alineador de dirección
     <h2/>
-    <p style="font-size: 14px;">
-      Una máquina de alineación de dirección es un equipo utilizado para medir y ajustar los ángulos de las ruedas de un vehículo, asegurando que estén correctamente alineadas según las especificaciones del fabricante. Su función principal es optimizar la estabilidad, el control y la vida útil de los neumáticos, evitando desgastes irregulares y mejorando la eficiencia del combustible.
+    <p style="font-size: 16px; padding: 24px; text-align: justified;">
+      Diseñada para calibrar el ángulo de las ruedas, evitando el desgaste irregular de los neumáticos, y garantizando una conducción segura y estable.
     </p>
   </div>
 `;
@@ -70,12 +80,12 @@ const elevador_hidraulico = new PANOLENS.Infospot(64, 'images/panolense/car-jack
 elevador_hidraulico.position.set(-500, -38, 468);
 elevador_hidraulico.addHoverText("", -256);
 elevador_hidraulico.element.innerHTML =`
-  <div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 400px;">
-    <h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">
+  <div style="background-color: rgba(0, 0, 0, 0.8); border: 1px solid rgba(255, 255, 255, 0.8); color: #fff; border-radius: 15px; width: 400px;">
+    <h2 style="text-align: center; font-size: 22px; padding: 20px; width: 100%; background-color: white; color: black; border-radius: 15px 15px 0px 0px;">
       Elevador hidraulico
     <h2/>
-    <p style="font-size: 14px;">
-      Un elevador hidráulico de plataforma es un equipo utilizado en talleres mecánicos para elevar vehículos de manera segura y estable, facilitando tareas de mantenimiento, reparación y alineación. Funciona mediante un sistema hidráulico que acciona cilindros de elevación, permitiendo al operador levantar el automóvil con poco esfuerzo.
+    <p style="font-size: 16px; padding: 24px; text-align: justified;">
+      Permite levantar vehículos con facilidad y seguridad para realizar mantenimiento, inspecciones o reparaciones en la parte inferior. 
     </p>
   </div>
 `;
@@ -84,11 +94,11 @@ const prensa_hidraulica = new PANOLENS.Infospot(48, 'images/panolense/hydraulic-
 prensa_hidraulica.position.set(-500 ,-87 , -176);
 prensa_hidraulica.addHoverText("", -256);
 prensa_hidraulica.element.innerHTML =`
-  <div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 400px;">
-    <h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">
+  <div style="background-color: rgba(0, 0, 0, 0.8); border: 1px solid rgba(255, 255, 255, 0.8); color: #fff; border-radius: 15px; width: 400px;">
+    <h2 style="text-align: center; font-size: 22px; padding: 20px; width: 100%; background-color: white; color: black; border-radius: 15px 15px 0px 0px;">
       Prensa hidraulica
     <h2/>
-    <p style="font-size: 14px;">
+    <p style="font-size: 16px; padding: 24px; text-align: justified;">
       Funciona mediante un sistema de cilindros y fluido hidráulico, y se emplea comúnmente para tareas como extraer e instalar rodamientos, bujes, engranajes, y enderezar piezas metálicas. 
     </p>
   </div>
@@ -98,11 +108,11 @@ const horario = new PANOLENS.Infospot(48, 'images/panolense/clock.png');
 horario.position.set(500 ,-44 , -8);
 horario.addHoverText("", -226);
 horario.element.innerHTML =`
-  <div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 260px;">
-    <h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">
+  <div style="background-color: rgba(0, 0, 0, 0.8); border: 1px solid rgba(255, 255, 255, 0.8); color: #fff; border-radius: 15px; width: 260px;">
+    <h2 style="text-align: center; font-size: 22px; padding: 20px; width: 100%; background-color: white; color: black; border-radius: 15px 15px 0px 0px;">
       Horario de atención
     <h2/>
-    <p style="font-size: 16px; pa">
+    <p style="font-size: 16px; padding: 24px; text-align: center;">
       Lun-Vie: 8:00 am - 5:00 pm<br>
       Sáb: 9:00 am - 4:00 pm<br>
       Dom: Cerrado
@@ -113,13 +123,13 @@ horario.element.innerHTML =`
 const gabinete_herramientas = new PANOLENS.Infospot(64, 'images/panolense/tool.png');
 const videoId = "gabineteVideo";
 gabinete_herramientas.position.set(-500 ,-129 , 101);
-gabinete_herramientas.addHoverText("", -60);
+gabinete_herramientas.addHoverText("", 100);
 gabinete_herramientas.element.innerHTML = `
-  <div style="background-color: rgba(0, 0, 0, 0.8); color: rgb(255, 255, 255); border-radius: 5px; padding: 10px; font-size: 14px; width: 720px;">
-    <h1 style="color: rgb(255, 255, 255); text-align: center;">
+  <div style="background-color: rgba(0, 0, 0, 0.8); border: 1px solid rgba(255, 255, 255, 0.8); color: #fff; border-radius: 15px; width: 428px; overflow: hidden;">
+    <h2 style="text-align: center; font-size: 22px; padding: 20px; width: 100%; background-color: white; color: black; border-radius: 15px 15px 0px 0px;">
       Gabinete de herramientas
-    </h1>  
-    <iframe id="${videoId}" width="720" height="480" src="https://www.youtube.com/embed/df4_C9ez3oQ?enablejsapi=1" title="Gabinetes configurables Knova" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </h1> 
+      <iframe id="${videoId}" width="426" height="240" src="https://www.youtube.com/embed/df4_C9ez3oQ?enablejsapi=1" title="Gabinetes configurables Knova" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
   </div>
 `;
 
@@ -127,7 +137,7 @@ const encendido_auto = new PANOLENS.Infospot(48, 'images/panolense/horn.png');
 encendido_auto.position.set(131 ,-73 , 500);
 encendido_auto.addHoverText("", 60);
 encendido_auto.element.innerHTML = `
-  <div style="background-color: rgba(0, 0, 0, 0.8); color: #000; border-radius: 100px; padding: 40px; font-size: 14px;">
+  <div style="background-color: rgba(0, 0, 0, 0.8); color: #000; border-radius: 100px; padding: 40px; font-size: 16px; border: 1px solid white;">
         <audio controls>
             <source src="media/car-ignition.mp3" type="audio/mpeg">
             Tu navegador no soporta el elemento de audio.
@@ -135,20 +145,40 @@ encendido_auto.element.innerHTML = `
     </div>
 `;
 
-const info = new PANOLENS.Infospot(48, 'images/panolense/information-button.png');
+const info = new PANOLENS.Infospot(24, 'images/panolense/information-button.png');
 info.position.set(246 ,73 , -500);
-info.addHoverText("", -256);
+info.addHoverText("", -236);
 info.element.innerHTML =`
-  <div style="background-color: rgba(0, 0, 0, 0.8); color: #fff; border-radius: 5px; padding: 20px; width: 400px;">
-    <h2 style="text-align: center; font-size: 18px; padding-bottom: 20px;">
+  <div style="background-color: rgba(0, 0, 0, 0.8); border: 1px solid rgba(255, 255, 255, 0.8); color: #fff; border-radius: 15px; width: 400px;">
+    <h2 style="text-align: center; font-size: 22px; padding: 20px; width: 100%; background-color: white; color: black; border-radius: 15px 15px 0px 0px;">
       Hoja de recepción en taller
     <h2/>
-    <p style="font-size: 14px;">
+    <p style="font-size: 16px; padding: 24px; text-align: center;">
       Haz clic en el icono para ver ejemplo de hoja de recepcion en taller.
     </p>
   </div>
 `;
 info.addEventListener('click', function() {window.open("documents/ht-taller.pdf", "_blank");});
+
+const workshop_front = new PANOLENS.Infospot(48, 'images/panolense/image.png');
+workshop_front.position.set(500 ,140 , -6);
+workshop_front.addHoverText("", -236);
+workshop_front.element.innerHTML =`
+  <div style="background-color: rgba(0, 0, 0, 0.8); border: 1px solid rgba(255, 255, 255, 0.8); color: #fff; border-radius: 15px; width: 400px;">
+    <h2 style="text-align: center; font-size: 22px; padding: 20px; width: 100%; background-color: white; color: black; border-radius: 15px 15px 0px 0px;">
+      Imagen de taller
+    <h2/>
+    <p style="font-size: 16px; padding: 24px; text-align: center;">
+      Haz clic en el icono para abrir la imagen.
+    </p>
+  </div>
+`;
+workshop_front.addEventListener('click', function() {
+  workshop_front_container.style.display = "block";
+  workshop_front_img.src = workshop_front_img.src;
+  workshop_front_alt.innerHTML = workshop_front_img.alt;
+});
+
 
 function controlVideo(action) {
   const iframe = document.getElementById(videoId);
@@ -169,6 +199,7 @@ panorama.add(alineacion_direccion);
 panorama.add(balanceador_llantas);
 panorama.add(elevador_hidraulico);
 panorama.add(prensa_hidraulica);
+panorama.add(workshop_front);
 panorama.add(encendido_auto);
 panorama.add(destalonadora);
 panorama.add(horario);
